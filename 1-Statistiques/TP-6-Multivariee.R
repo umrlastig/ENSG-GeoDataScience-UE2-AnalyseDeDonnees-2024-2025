b4fa4507c16641cda1876d4d2e271a3a53020886
Juste Raimbault <juste.raimbault@ign.fr>
2024-11-14 09:11:16 +0100

    update TP6

--- 1-Statistiques/TP-6-Multivariee.R
+++ 1-Statistiques/TP-6-Multivariee.R
@@ -1,17 +1,26 @@
 
 # TP 6 : analyse multivariée
 
-
+#######################
 # 1) Regression multiple
+#######################
 
+# 1.1) Préparation des données
 # Charger à nouveau les données des matches de LOL,
 #   filtrer les résultats d'équipe,
 #   selectionner quelques variables quantitatives (dont result,gamelength)
 
 
 
+
+
+
+# 1.2) Modèles linéaires multivariés
+
 # Utiliser des modèles linéaires pour expliquer dpm,result,gamelength (fonction lm)
 
+
+# Comparer ces modèles en termes de R2, de AIC
 
 
 # Idem en séparant victoires et défaites
@@ -26,14 +35,21 @@
 
 
 
+# 1.3) Régression logistique
 
-# Expliquer la variable binaire "result" avec une régression logistique (fonction glm)
-#   (pour obtenir les effets marginaux du modèle logit, utiliser la fonction logitmfx du package mfx)
+# Expliquer la variable binaire "result" avec une régression logistique (fonction glm, en utilisant family = binomial(link = 'logit'))
+#    - pour obtenir les effets marginaux du modèle logit, utiliser la fonction logitmfx du package mfx
+#    - comparer le modèle logit avec un modèle linéaire, en utilisant le pseudo R2 de McFadden = 1 - logLik(logit)/logLik(model nul avec result~1)
 
 
 
 
+
+#######################
 # 2) Séries temporelles
+#######################
+
+
 
 # Vérifier s'il existe une causalité de Granger entre les variables observées à étapes intermédiaires (kills, golddiff, etc at 10, 15) et le résultat
 
@@ -44,12 +60,15 @@
 
 
 
-
+#######################
 # 3) Reduction de dimensionalité
-
+#######################
 
 # Prendre en main les méthodes t-SNE (package Rtsne) et UMAP (package umap)
 
 
 
 # Comparer PCA, t-SNE, et UMAP sur un ensemble de variables quantitatives (joueurs et/ou équipes)
+
+
+
