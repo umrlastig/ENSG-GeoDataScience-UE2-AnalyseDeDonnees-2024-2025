7f178b074f194345d25bd43b0c23730cae0569d3
Juste Raimbault <juste.raimbault@ign.fr>
2024-12-13 15:24:15 +0100

    TP10_reseaux

--- 3-Reseaux/TP-10-Reseaux_CORRECTION.R
+++ 3-Reseaux/TP-10-Reseaux_CORRECTION.R
@@ -194,14 +194,17 @@ nodes <- read_csv("https://raw.githubusercontent.com/mathbeveridge/asoiaf/master
 edges <- read_csv("https://raw.githubusercontent.com/mathbeveridge/asoiaf/master/data/asoiaf-all-edges.csv")
 
 
-
-
 # construire le graphe: graph_from_data_frame
-
+g = graph_from_data_frame(edges,directed = F)
 
 
 # 2.2) ploter le graph avec un layout adapte
+plot(g, vertex.size=1, vertex.label = NA)
 
+coords = layout_with_fr(g)
+V(g)$x = coords[,1]
+V(g)$y = coords[,2]
+plot(g, vertex.size=1, vertex.label = NA)
 
 
 # pour bien visualiser: gephi, par exemple apres export en gml
@@ -217,35 +220,73 @@ edges <- read_csv("https://raw.githubusercontent.com/mathbeveridge/asoiaf/master
 
 # 2.3) distribution des degres
 
+degree(g)
+deg_pondere = strength(g)
+deg_pondere[deg_pondere>quantile(deg_pondere,0.99)]
 
 # histogramme
-
+hist(deg_pondere, breaks=200)
 
 # loi rang-taille : log(degre) ~ log(rang)
+plot(log(1:length(V(g))), sort(log(strength(g)), decreasing = T))
 
 
 
 # ajuster des power law avec plus de parametres, ou des distributions log-normale
 # package poweRlaw
+library(poweRlaw)
+wdeg_estimator = poweRlaw::conpl$new(deg_pondere)
+est = poweRlaw::estimate_xmin(wdeg_estimator,xmax = max(deg_pondere))
+wdeg_estimator$setXmin(est)
 
+wdeg_estimator_lnorm = poweRlaw::conlnorm$new(deg_pondere)
+est_lnorm = poweRlaw::estimate_xmin(wdeg_estimator_lnorm,xmax = max(deg_pondere))
+wdeg_estimator_lnorm$setXmin(est_lnorm)
+
+plot(wdeg_estimator);lines(wdeg_estimator, col=2, lwd=2);lines(wdeg_estimator_lnorm, col=3, lwd=2)
 
 
 
 # 2.4) centralites : closeness, betwenness, eigenvalue
+clos = closeness(g)
+clos[clos>quantile(clos,0.99)]
 
+betw = betweenness(g)
+betw[betw>quantile(betw,0.99)]
+
+cor.test(clos,betw)
+cor.test(deg_pondere,betw)
+
+eig = eigen_centrality(g)$vector
+eig[eig>quantile(eig,0.99)]
 
 
 # role de la centralité pour expliquer le nombre d'occurences?
-
+cor.test(betw, deg_pondere)
+cor.test(clos, deg_pondere)
+cor.test(eig, deg_pondere)
 
 
 # 2.5) detection de communautes : cluster_... -> methode de Louvain cluster_louvain
+coms = cluster_fast_greedy(g)
+coms
 
+coms = cluster_louvain(g)
+coms
 
 
 
 # 2.6) plotter avec multiples infos: communaute, centralite, degre
 # (export dans le fichier "./graph.png")
+png('graph.png',width=20,height = 20, units='cm',res=300)
+plot(
+  g,
+  vertex.size = 3+log(strength(g,weights = E(g)$occs))/2,
+  vertex.frame.color = NA,
+  vertex.color = coms$membership,
+  vertex.label.cex = clos #eig/2
+)
+dev.off()
 
 
 
